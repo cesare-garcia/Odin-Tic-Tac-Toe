ddb1249ecdf8b927fccb5883f6370478e87b52fe
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-03-20 20:50:16 -0500

    restart creation of game module

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-var selectStart = (function(){
-    let startButton = document.querySelector("#start");
-    startButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        game.start();
-        document.querySelector("#new_game").disabled = false;
-    });
-    return { startButton };
-})();
-
-var selectReset = (function(){
-    let newGameButton = document.querySelector("#new_game");
-    newGameButton.addEventListener("click", (e) => {
-        alert("Resetting Page");
-    });
-})();
-
-var gameBoard = (function(){
+var gameBoard = ( () => {
 
     let board = ["","","","","","","","",""];
 
-    let render = function() {
+    let checkBoard = function() {
+        console.log(board);
+    };
+
+    const render = function() {
 
         if ( document.querySelector(".grid") == undefined ) {
             let main = document.querySelector("main");
@@ -38,18 +25,23 @@ var gameBoard = (function(){
             let boxes = document.querySelectorAll(".gridBox");
             boxes.forEach( (element) => {
                 element.addEventListener("click", (e) => {
-                    console.log(e.target.id);
+                    console.log(e.target.id); // this will need to be currentTurn.marker
                 });
             });
         };
     };
 
-    let checkBoard = function() {
-        console.log(board);
+    let update = function() {
+        // let boxes = document.querySelectorAll(".gridBox");
+        // boxes.forEach( (element) => {
+        //     element.addEventListener("click", (e) => {
+        //         e.target.innerText = "X"; // this will need to be currentTurn.marker
+        //     });
+        // });
+
     };
 
-    // the update function that is returned will be the one that allows board to be adjusted.
-    return { checkBoard, render };
+    return { checkBoard, render, update };
 
 })();
 
@@ -57,30 +49,17 @@ function playerFactory(name, token) {
     return {name, token};
 }
 
-var game = (function(){
-    
-    let start = function() {
-
-        if ( document.querySelector("#player1").value != "" && document.querySelector("#player2").value != "" ) {
-            var playerOne = playerFactory(document.querySelector("#player1").value,"X");
-            var playerTwo = playerFactory(document.querySelector("#player2").value, "O");
-            gameBoard.render();
-            selectStart.startButton.disabled = "true";
-            let form = document.querySelector("form");
-            form.reset();
-
-            return { playerOne, playerTwo };
-
-        } else {
-            alert("You must enter names for both players to start the game.");
-        }
-    };
-
-    
-
-
-
-
-    return { start };
+const game = (() => {
 
 })();
+
+
+
+
+
+
+
+const startButton = document.querySelector("#start");
+startButton.addEventListener("click", (e) => {
+    console.log("testing");
+});
